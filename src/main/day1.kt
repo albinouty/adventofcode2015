23862cbe1bf74a09b3bf412915111b77e3afd155
albinouty <rickutykanski@gmail.com>
2020-12-06 14:26:55 -0700

    day 1

--- src/main/day1.kt
+++ src/main/day1.kt
@@ -1,5 +1,33 @@
 package main
 
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("hello world!")
+    fun readFileAsLinesUsingUseLines(fileName: String) = File(fileName).useLines { it.toList() }
+    val inputDayOne = readFileAsLinesUsingUseLines("src/main/resources/inputday1.txt")
+
+    val floors = inputDayOne[0].map {
+        if(it == '(') {
+            1
+        }
+        else -1
+    }
+    //part 1
+    println("Santa needs to go to floor ${floors.sum()}")
+
+    //part 2
+    var ans = 0
+    var x = 0
+    for(i in floors) {
+        if(ans < 0) {
+            break
+        } else if(i == 1) {
+            ans += i
+            x += 1
+        } else {
+            ans -= 1
+            x += 1
+        }
+    }
+    println(x)
 }
